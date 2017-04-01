a8252ea2756d1f7c1a7e6aa0223588e54f561d03
Daniel Tayler <danieltayler@mail.weber.edu>
2017-04-01 23:04:49 +0000

    start of file

---
+++ Barjoran_hw8_task2.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Barjoran_hw8_task2.c
+ *       	Usage:  ./Barjoran_hw8_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/01/2017 11:02:09 PM
+ *
+ Compiler:  gcc -Wall -Werror -lm 
+ *         Author:  Daniel Tayler (), danieltayler@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+#include <string.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int  main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
