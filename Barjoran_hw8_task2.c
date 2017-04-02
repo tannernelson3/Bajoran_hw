5f3c3fb7225556e8baecf57d6da84254ba41e7f8
Daniel Tayler <danieltayler@mail.weber.edu>
2017-04-02 00:00:59 +0000

    think i have an idea were we are going

--- Barjoran_hw8_task2.c
+++ Barjoran_hw8_task2.c
@@ -16,16 +16,77 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 81
 /* Function Prototypes */
+void Usage(void); 
+void getOptions(char task);
+void PrintOption(char *string);
 
 /* Main Program */
 int  main(int argc, char *argv[])
 {
+
+	char task;
+	char str[MAX];
+	task = argv[1];
+
+
+	if (argc != 2)
+	{
+		printf("you don't have the correct number of perameters plz check again, you should have two\n");
+	}
+	else if ( task != 'p' ||task !='l' ||task != 'u')
+	{
+		printf(" did use right stype default now is p\n");
+		task = 'p';
+	} 
+	getOptions(task);
+	PrintOption(str);
 
 	return 0;
 }
 
 
 /* Function Defenitions */
+void Usage(void) 
+{
+	exit (1);
+	return;
+}
 
 
+void getOptions(char task)
+{
+	printf("please enter someithng until you want ot end which is crt d");
+	str =getchar();
+	if (task == 'p')
+	{
+			printf("we've left it alone like you wanted");
+	}
+	else if (task == 'l')
+	{
+		for ( int i = 0; i < sizeof(str)/sizeof(char) ; i++)
+		{
+			str[i]= toupper(str[i]);
+		}
+
+	}
+	else if (task == 'u')
+	{
+		for ( int i = 0; i < sizeof(str)/sizeof(char) ; i++)
+		{
+			str[i]= tolower(str[i]);
+		}
+
+	}
+	return;
+}
+
+void PrintOption(char *string)
+{
+		for ( int i = 0; i < sizeof(string)/sizeof(char) ; i++)
+		{
+			printf("%c" , string[i]);
+		}
+	return;
+}
